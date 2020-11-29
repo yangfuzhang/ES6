2fb3626213eaef4f2f91bcf142736b092f84dd5b
yangfuzhang <yangfuzhang0720@126.com>
2020-11-29 08:35:45 +0800

    add：new file

--- add.js
+++ add.js
@@ -2,25 +2,25 @@
  * 实现add(1)(2, 3)(4).value()
  */
 
- function add() {
- 	const args = [].slice.apply(arguments)
- 	const self = this
+function add() {
+	const args = [].slice.apply(arguments)
+	const self = this
 
- 	self.nums = [...args]
+	self.nums = [...args]
 
- 	function _add() {
- 		const _args = [].slice.apply(arguments)
+	function _add() {
+		const _args = [].slice.apply(arguments)
 
- 		self.nums.push(..._args)
+		self.nums.push(..._args)
 
- 		return _add
- 	}
+		return _add
+	}
 
- 	_add.value = function() {
- 		return self.nums.reduce((acc, cur) => {
-            return acc += cur
- 		}, 0)
- 	}
+	_add.value = function() {
+    return self.nums.reduce((acc, cur) => {
+        return acc += cur
+		}, 0)
+	}
 
- 	return _add
- }
+    return _add
+}
