248e2d61398839940d433bc35840ab5ea54994b7
yangfuzhang <yangfuzhang0720@126.com>
2020-12-29 05:12:18 +0800

    add：new file

---
+++ x的平方根.js
@@ -0,0 +1,22 @@
+let squareRoot = function(x) {
+    let left = 0
+    let right = x
+
+    let ans = -1
+
+    while(left <= right) {
+    	let mid = left + Math.round((right - left) / 2)
+    	let product = mid * mid
+
+    	if(product < x) {
+    		ans = mid
+    		left = mid + 1
+    	} else if(product > x) {
+    		right = mid - 1
+    	} else {
+    		return mid
+    	}
+    }
+
+    return ans
+}
